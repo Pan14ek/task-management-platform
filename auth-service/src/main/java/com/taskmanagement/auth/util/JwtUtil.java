af6ddd2bedda625d2b00b9ada1420e1d8cd95b04
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-02-13 10:50:39 +0200

    Add the security configuration that users can use specific REST APIs

--- auth-service/src/main/java/com/taskmanagement/auth/util/JwtUtil.java
+++ auth-service/src/main/java/com/taskmanagement/auth/util/JwtUtil.java
@@ -7,24 +7,34 @@ import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-  private static final String SECRET_KEY = "mySuperSecretKey1234567890mySuperSecretKey";
-  private static final long EXPIRATION_TIME = 15 * 60 * 1000;
+  private final long jwtExpiration;
+  private final Key key;
 
-  private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+  public JwtUtil(@Value("${jwt.secret}") String secretKey,
+                 @Value("${jwt.expiration}") long jwtExpiration) {
+    this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    this.jwtExpiration = jwtExpiration;
+  }
 
   public String generateAccessToken(UUID userId, String username) {
     return Jwts.builder()
         .setSubject(userId.toString())
         .claim("username", username)
         .setIssuedAt(new Date())
-        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+        .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
         .signWith(key, SignatureAlgorithm.HS256)
         .compact();
+  }
+
+  public String getUsernameFromToken(String token) {
+    return Jwts.parser().setSigningKey(key).build()
+        .parseClaimsJws(token).getBody().getSubject();
   }
 
   public boolean validateToken(String token) {
