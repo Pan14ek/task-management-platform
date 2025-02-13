344e44592c4bcec05e88c86786f95279345747a6
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-02-13 11:13:16 +0200

    Add new SQL queries

--- auth-service/src/main/resources/database/scripts/schema.sql
+++ auth-service/src/main/resources/database/scripts/schema.sql
@@ -1,26 +1,38 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS users (
-                                     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                                     username VARCHAR(50) UNIQUE NOT NULL,
-                                     password TEXT NOT NULL,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS users
+(
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username   VARCHAR(50) UNIQUE NOT NULL,
+    password   TEXT               NOT NULL,
+    created_at TIMESTAMP        DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS roles (
-                                     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                                     name VARCHAR(50) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS roles
+(
+    id   UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_roles (
-                                          user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-                                          role_id UUID REFERENCES roles(id) ON DELETE CASCADE,
-                                          PRIMARY KEY (user_id, role_id)
+CREATE TABLE IF NOT EXISTS user_roles
+(
+    user_id UUID REFERENCES users (id) ON DELETE CASCADE,
+    role_id UUID REFERENCES roles (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, role_id)
 );
 
-CREATE TABLE IF NOT EXISTS refresh_tokens (
-                                              id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                                              user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-                                              token TEXT UNIQUE NOT NULL,
-                                              expiry_date TIMESTAMP NOT NULL
+CREATE TABLE IF NOT EXISTS refresh_tokens
+(
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id     UUID REFERENCES users (id) ON DELETE CASCADE,
+    token       TEXT UNIQUE NOT NULL,
+    expiry_date TIMESTAMP   NOT NULL
 );
+
+INSERT INTO roles (id, name)
+SELECT uuid_generate_v4(), 'ROLE_USER'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'ROLE_USER');
+
+INSERT INTO roles (id, name)
+SELECT uuid_generate_v4(), 'ROLE_ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'ROLE_ADMIN');
