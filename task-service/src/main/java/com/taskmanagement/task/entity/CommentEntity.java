59a65c0220204d391a762a0de8aabcb816fd26f8
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-02-24 11:06:46 +0200

    Update the comment entity

--- task-service/src/main/java/com/taskmanagement/task/entity/CommentEntity.java
+++ task-service/src/main/java/com/taskmanagement/task/entity/CommentEntity.java
@@ -1,6 +1,7 @@
 package com.taskmanagement.task.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -19,10 +20,13 @@ public class CommentEntity {
   @Id
   private UUID id;
 
+  @Column(nullable = false, name = "user_id")
   private UUID userId;
 
+  @Column(nullable = false, name = "content")
   private String content;
 
+  @Column(name = "created_at")
   private LocalDateTime createdAt;
 
   @ManyToOne
