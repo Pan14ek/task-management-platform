4227182ef50710458c693278135dbe03ce176dcf
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-02-24 10:49:58 +0200

    Update docker compose files to have 2 separate dev databases

--- task-service/src/main/resources/database/scripts/schema.sql
+++ task-service/src/main/resources/database/scripts/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS tasks (
                                      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                                      title VARCHAR(255) NOT NULL,
