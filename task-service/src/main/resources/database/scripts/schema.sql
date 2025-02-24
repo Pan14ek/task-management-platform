284cbd1a1dea8617a386a64a9a4c7c6f5949b6c8
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-02-24 10:17:22 +0200

    Update the schema sql

---
+++ task-service/src/main/resources/database/scripts/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS tasks (
+                                     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                                     title VARCHAR(255) NOT NULL,
+                                     description TEXT,
+                                     status VARCHAR(50) DEFAULT 'TO_DO',
+                                     priority INT DEFAULT 1,
+                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                     due_date TIMESTAMP,
+                                     assigned_user UUID NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+                                        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                                        task_id UUID REFERENCES tasks(id) ON DELETE CASCADE,
+                                        user_id UUID NOT NULL,
+                                        content TEXT NOT NULL,
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
