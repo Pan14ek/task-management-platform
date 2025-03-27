e511502e93d46b3a502845252c5196fb824974ee
Oleksii Makieiev <alexmakeev2703@gmail.com>
2025-03-27 10:02:28 +0200

    task: Add ApplicationConfig classes to the auth and task services

---
+++ task-service/src/main/java/com/taskmanagement/task/config/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.taskmanagement.task.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.JacksonUtils;
+
+@Configuration
+public class ApplicationConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return JacksonUtils.enhancedObjectMapper();
+  }
+
+}
